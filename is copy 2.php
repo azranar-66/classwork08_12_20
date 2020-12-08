7064324db3aa31cbfde50c70c3c99bea5d631031
azranar-66 <vorgen95@gmail.com>
2020-12-08 21:21:34 +0300

    php alharitm

---
+++ is copy 2.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    $x = 5;
+    $y = $x ** 2;
+    echo $y;
+    ?>
+    <?php
+
+    $x = 4;
+    $y = sqrt($x) / $x ** 2;
+    echo $y;
+
+
+    ?>
+    <?php
+
+    $a = 4;
+    $b = 3;
+    $y = abs($a - $b) / sin($b) * sqrt($b + $a);
+    echo $y;
+
+
+    ?>
+    
+
+
+
+</body>
+
+</html>
---
+++ is.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    $a = 11;
+    $b = 15;
+    $c = 17;
+    $d = 13;
+    echo "<br>\n";
+    echo $a . $b;
+    ?>
+    <?php
+    echo "<br>\n";
+    $a = "11";
+    $b = "15";
+    echo $a + $b;
+
+    ?>
+    <?php
+    echo "<br>\n";
+    $a = ($b = 4) + 5;
+    echo  $a;
+
+    ?>
+    <?php
+    echo "<br>\n";
+    $a =  5;
+    echo  $a."<br>";
+    $a+=2;
+    echo  $a."<br>";
+
+    $a-=3;
+    echo  $a."<br>";
+    $a .=4;
+    echo  $a."<br>";
+    ?>
+     <?php
+    echo "<br>\n";
+    $a =  5;
+    echo  $a."<br>";
+    $a+=2;
+    echo  $a."<br>";
+
+    $a-=3;
+    echo  $a."<br>";
+    $a .=4;
+    echo  $a."<br>";
+    ?>
+
+
+</body>
+
+</html>
